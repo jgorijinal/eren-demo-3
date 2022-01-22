0ed54ab3e0b497566793b0eae4b820e959d3f883
jgoriginal <932274204@qq.com>
2022-01-22 17:01:27 +0800

    完成button-group , icon.vue

--- src/button.vue
+++ src/button.vue
@@ -1,25 +1,51 @@
 <template>
-<button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click',!loading)">
+    <g-icon v-if="loading " class="loading" name="loading"></g-icon>
+    <g-icon v-if="icon && !loading" class="icon" :name="icon"></g-icon>
+    <div class="content">
+      <slot/>
+    </div>
+  </button>
 </template>
 <script>
-export default {}
+
+export default {
+  props: {
+    icon:String,
+    iconPosition:{
+      type:String,
+      default:'left',
+      validator(value) {
+        return value === 'left' || value === 'right'
+      }
+    },
+    loading:{
+      type:Boolean,
+      default: false
+    }
+  },
+
+}
 </script>
 <style lang="scss">
 .g-button {
-  font-size: var(--font-size);
-  height: var(--button-height);
-  padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
-  &:hover {
-  border-color: var(--border-color-hover);
-}
-  &:active {
-  background-color: var(--button-active-bg);
-}
-  &:focus {
-  outline: none;
-}
+  font-size: var(--font-size);height: var(--button-height);padding: 0 1em;border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);background: var(--button-bg);display:inline-flex;justify-content: center;
+  align-items: center;vertical-align: middle;
+  &:hover {border-color: var(--border-color-hover);}
+  &:active {background-color: var(--button-active-bg);}
+  &:focus {outline: none;}
+  > .icon {order: 1;margin-right: 0.5em;margin-left: 0;}
+  > .content {order: 2}
+  &.icon-right {
+    > .icon {order: 2;margin-right: 0;margin-left: 0.5em;}
+    > .content {order: 1;} }
+  .loading{
+    animation: spin 2s infinite linear;
+  }
+  @keyframes spin {
+    0% {transform: rotate(0)}
+    100% {transform: rotate(360deg)}
+  }
 }
 </style>
