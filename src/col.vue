cc005154a6f9073bd1beea364814887d9b113789
jgoriginal <932274204@qq.com>
2022-01-24 22:27:22 +0800

    row和col的雏形完成

---
+++ src/col.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props:{
+    span:{
+      type:[Number,String]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.col {
+  height: 100px;
+  border: 1px solid red;
+  background: grey;
+  width:50%;
+  $class-prefix:col-;
+  @for $n from 1 through 24  {
+    &.#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%
+    }
+  }
+}
+</style>
