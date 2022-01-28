a71569f2ef9f822d5581bcb74f8c1b3d8dd06d4d
jgoriginal <932274204@qq.com>
2022-01-28 23:45:47 +0800

    实现了只能有一个 toast 的功能

--- src/plugin.js
+++ src/plugin.js
@@ -1,16 +1,42 @@
 import Toast from './toast.vue'
 
+let currentToast
+
 export default {
     install(Vue, options) {
         Vue.prototype.$toast = function (message,toastOptions) {
-            const Constructor = Vue.extend(Toast)  //创建动态实例
-            const toast = new Constructor({
-                propsData:toastOptions
+            if(currentToast){
+                currentToast.close()
+            }
+           currentToast =  createToast({Vue,message,propsData:toastOptions})
 
-            })        //创建动态实例
-            toast.$slots.default = [message]    //插槽slot里面写内容
-            toast.$mount()                         //挂
-            document.body.appendChild(toast.$el)   //添加到页面
         }
     }
 }
+/*helpers*/
+function createToast({Vue,message,propsData}){
+    const Constructor = Vue.extend(Toast)  //创建动态实例
+    const toast = new Constructor({
+        propsData
+    })        //创建动态实例
+    toast.$slots.default = [message]    //插槽slot里面写内容
+    toast.$mount()                         //挂
+    document.body.appendChild(toast.$el)   //添加到页面
+    return toast
+}
+
+
+
+
+
+// Vue.prototype.$toast = function (message,toastOptions) {
+//             function x(){
+//                 const Constructor = Vue.extend(Toast)  //创建动态实例
+//                 const toast = new Constructor({
+//                     propsData:toastOptions
+//                 })        //创建动态实例
+//                 toast.$slots.default = [message]    //插槽slot里面写内容
+//                 toast.$mount()                         //挂
+//                 document.body.appendChild(toast.$el)   //添加到页面
+//             }
+//             x()
