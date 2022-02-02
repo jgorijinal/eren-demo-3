34006fa65f9b8cb3736e18f74a2a051fe26b8813
jgoriginal <932274204@qq.com>
2022-02-02 15:38:12 +0800

    collapse基本样式和基本功能完成

---
+++ src/collapse-item.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="collapseItem">
+  <div class="title" @click="open = !open ">{{title}}</div>
+  <div class="content" v-if="open"><slot></slot></div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "GuluCollapseItem",
+  props:{
+    title:{
+      type:String,
+      required:true
+    }
+  },
+  data(){
+    return {
+      open:false
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.collapseItem {
+  > .title {
+    border: 1px solid #999999;
+    margin-top: -1px;
+    margin-left: -1px;
+    margin-right: -1px;
+    padding: 12px 16px 12px 40px;
+    cursor: pointer;
+  }
+
+  > .content {
+    padding: 16px;;
+  }
+
+  &:first-child {
+    > .title {
+      border-top-left-radius: 4px;
+      border-top-right-radius: 4px;
+    }
+  }
+  &:last-child {
+    > .title:last-child {
+      border-bottom-left-radius:4px;
+      border-bottom-right-radius:4px;
+      margin-bottom: -1px;
+    }
+  }
+}
+
+</style>
---
+++ src/collapse.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="collapse">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GuluCollapse",
+}
+</script>
+
+<style lang="scss" scoped>
+.collapse {
+  border: 1px solid #999999;
+  border-radius: 4px;
+}
+</style>
